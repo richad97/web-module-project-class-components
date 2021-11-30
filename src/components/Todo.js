9638c45ab63971b63f0af16bdf5d0cd675f14b92
richad.97 <rcastillo2022@gmail.com>
2021-11-30 08:35:56 -0800

    completed requirements

---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class Todo extends React.Component {
+  handleClick = () => {
+    this.props.handleToggle(this.props.todo.id);
+  };
+  render() {
+    return (
+      <li onClick={this.handleClick}>
+        {this.props.todo.task}{" "}
+        {this.props.todo.completed ? <span> Completed</span> : <span></span>}
+      </li>
+    );
+  }
+}
---
+++ src/components/TodoForm.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default class TodoForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.handleAdd(this.state.input);
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      ...this.state,
+      input: e.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <form>
+        <input onChange={this.handleChange} />
+        <button onClick={this.handleSubmit}>Add</button>
+      </form>
+    );
+  }
+}
